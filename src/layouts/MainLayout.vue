c6f0a9618257632d4a8033742c8cc5e3082b5087
Alexander Chernyakov <alexander.chernyakov@waveaccess.global>
2023-12-30 18:39:15 +0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      AOP
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+</script>
